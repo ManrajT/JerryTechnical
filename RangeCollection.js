fac51b3ed333327b230a35a4c75223d020367d9c
Manraj Tatla <ManrajT@gmail.com>
2019-03-03 21:15:48 -0500

    adds checkRange

--- RangeCollection.js
+++ RangeCollection.js
@@ -7,28 +7,54 @@
  * NOTE: Feel free to add any extra member variables/functions you like.
  */
 class RangeCollection {
-  /**
-   * Adds a range to the collection
-   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
-   */
-  add(range) {
-    // TODO: implement this
-  }
 
-  /**
-   * Removes a range from the collection
-   * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
-   */
-  remove(range) {
-    // TODO: implement this
-  }
+    /**
+     * Adds a range to the collection
+     * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+     */
+    add(range) {
+        this._checkRange(range);
 
-  /**
-   * Prints out the list of ranges in the range collection
-   */
-  print() {
-    return "1";
-  }
+        // TODO: implement this
+    }
+
+    /**
+     * Removes a range from the collection
+     * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+     */
+    remove(range) {
+        this._checkRange(range);
+        // TODO: implement this
+    }
+
+    /**
+     * Prints out the list of ranges in the range collection
+     */
+    print() {
+        return "1";
+    }
+
+    /**
+     * Private method for checking range input value.  Throws exceptions if problems found.
+     * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
+     */
+    _checkRange(range) {
+        if (!Array.isArray(range)) {
+            throw new Error("Expected an array but received " + range);
+        }
+        if (!(range.length === 2)) {
+            throw new Error("Expected two values in array but received " + range.length);
+        }
+        let start = range[0];
+        let end = range[1];
+        if (!(typeof start === "number" && typeof end === "number")) {
+            throw new Error("Expected both values to be numbers.  Start:" + start + ",End:" + end);
+        }
+        if (start > end) {
+            throw new Error("Expected start of range to be before or equal to end of range." +
+                "Start:" + start + ",End:" + end)
+        }
+    }
 }
 
 
--- RangeCollection.test.js
+++ RangeCollection.test.js
@@ -1,35 +1,85 @@
-const RangeCollection = require('./RangeCollection');
+const RangeCollection = require("./RangeCollection");
 
 const rc = new RangeCollection();
 
-test('givenTests', () => {
-  rc.add([1, 5]);
-  expect(rc.print()).toBe("[1, 5)");
+test("givenTests", () => {
+    rc.add([1, 5]);
+    expect(rc.print()).toBe("[1, 5)");
 
-  rc.add([10, 20]);
-  expect(rc.print()).toBe("[1, 5) [10, 20)");
+    rc.add([10, 20]);
+    expect(rc.print()).toBe("[1, 5) [10, 20)");
 
-  rc.add([20, 20]);
-  expect(rc.print()).toBe("[1, 5) [10, 20)");
+    rc.add([20, 20]);
+    expect(rc.print()).toBe("[1, 5) [10, 20)");
 
-  rc.add([20, 21]);
-  expect(rc.print()).toBe("[1, 5) [10, 21)");
+    rc.add([20, 21]);
+    expect(rc.print()).toBe("[1, 5) [10, 21)");
 
-  rc.add([2, 4]);
-  expect(rc.print()).toBe("[1, 5) [10, 21)");
+    rc.add([2, 4]);
+    expect(rc.print()).toBe("[1, 5) [10, 21)");
 
-  rc.add([3, 8]);
-  expect(rc.print()).toBe("[1, 8) [10, 21)");
+    rc.add([3, 8]);
+    expect(rc.print()).toBe("[1, 8) [10, 21)");
 
-  rc.remove([10, 10]);
-  expect(rc.print()).toBe("[1, 8) [10, 21)");
+    rc.remove([10, 10]);
+    expect(rc.print()).toBe("[1, 8) [10, 21)");
 
-  rc.remove([10, 11]);
-  expect(rc.print()).toBe("[1, 8) [11, 21)");
+    rc.remove([10, 11]);
+    expect(rc.print()).toBe("[1, 8) [11, 21)");
 
-  rc.remove([15, 17]);
-  expect(rc.print()).toBe("[1, 8) [11, 15) [17, 21)");
+    rc.remove([15, 17]);
+    expect(rc.print()).toBe("[1, 8) [11, 15) [17, 21)");
 
-  rc.remove([3, 19]);
-  expect(rc.print()).toBe("[1, 3) [19, 21)");
+    rc.remove([3, 19]);
+    expect(rc.print()).toBe("[1, 3) [19, 21)");
+});
+
+test("not an array should throw error", () => {
+    function addNotAnArray() {
+        rc.add("not an array");
+    }
+
+    expect(addNotAnArray).toThrowError("Expected an array but received ");
+});
+
+test("different than 2 elements should throw error", () => {
+    function threeElements() {
+        rc.add([0, 1, 2]);
+    }
+
+    expect(threeElements).toThrowError("Expected two values in array but received");
+
+    function zeroElements() {
+        rc.add([]);
+    }
+
+    expect(zeroElements).toThrowError("Expected two values in array but received");
+});
+
+test("array without numbers should throw error", () => {
+    function stringsInArray() {
+        rc.add(["ok", 1]);
+    }
+
+    expect(stringsInArray).toThrowError("Expected both values to be numbers.");
+});
+
+test("start after end should throw error", () => {
+    function startAfterEnd() {
+        rc.add([2, 1]);
+    }
+
+    expect(startAfterEnd).toThrowError("Expected start of range to be before or equal to end of range.");
+
+    function startAfterEndNegatives() {
+        rc.add([0, -1]);
+    }
+
+    expect(startAfterEndNegatives).toThrowError("Expected start of range to be before or equal to end of range.");
+
+    function startEqualToEnd() {
+        rc.add([1, 1]);
+    }
+
+    expect(startEqualToEnd).not.toThrowError();
 });
